4b83ecd1ad858ad96aab3503c6346cae76cbcffa
Kundan Kumar <kundank@one.com>
2024-05-02 14:09:39 +0530

    gh page added

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,45 @@
+const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 const htmlPlugin = new HtmlWebPackPlugin({
- template: 'index.html',
- filename: './index.html'
+  template: 'index.html',
+  filename: './index.html'
 });
-module.exports = {
-mode: 'development',
-  module: {
-    rules: [{
-   test: /\.js$/,
-   exclude: /node_modules/,
-   use: {
-     loader: 'babel-loader'
-   }
- },
-  {
-   test: /\.css$/,
-   use: ['style-loader', 'css-loader']
-  }
-]},
- plugins: [htmlPlugin]
+
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isDevelopment ? 'bundle.js' : 'bundle.[contenthash].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    plugins: [htmlPlugin],
+    optimization: {
+      minimize: !isDevelopment
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map'
+  };
 };
